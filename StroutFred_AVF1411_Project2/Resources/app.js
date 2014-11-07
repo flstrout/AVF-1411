4afde1aa4d77887d78c56f72197fdcd611984a20
flstrout <FredStrout617@fullsail.edu>
2014-11-07 09:16:24 -0600

    Initial commit

--- StroutFred_AVF1411_Project2/Resources/app.js
+++ StroutFred_AVF1411_Project2/Resources/app.js
@@ -1,64 +1,24 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Titanium.UI.setBackgroundColor('#000');
+// AVF-1411 - Project 1a
+// Fred L. Strout
+// Created on: 10/30/2014
 
-// create tab group
-var tabGroup = Titanium.UI.createTabGroup();
+var getNetCheck = require("netCheck");
+var getLocation = require("location");
+
+Ti.Database.install("/database/localDataDB.sqlite", "localData");
+
+getLocation.currentLocation();
+//console.log(lat+","+lon);
+//var urlAPI = "http://api.wunderground.com/api/4af8df4427496b2f/conditions/forecast/q/LA/Rayne.json";
 
 
-//
-// create base UI tab and root window
-//
-var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
-    backgroundColor:'#fff'
-});
-var tab1 = Titanium.UI.createTab({  
-    icon:'KS_nav_views.png',
-    title:'Tab 1',
-    window:win1
-});
 
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
+//console.log(getLocation);
+
+//getNetCheck.checkNet(urlAPI);
+
+var window = Ti.UI.createWindow({
+	backgroundColor: "#09f"
 });
 
-win1.add(label1);
-
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
-});
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
-});
-
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win2.add(label2);
-
-
-
-//
-//  add tabs
-//
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
-
-
-// open tab group
-tabGroup.open();
+window.open();
